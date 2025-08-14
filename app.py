651e0326f49f66df6a5c636bbfdee44ecac857e9
Krunal patil <kunal586250@gmail.com>
2025-08-14 17:01:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -341,7 +341,8 @@ def index():
 
 if __name__ == "__main__":
     os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
-    port = int(os.environ.get("PORT", 5000))  # Render assigns a dynamic port
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
     app.run() 
 
                    
